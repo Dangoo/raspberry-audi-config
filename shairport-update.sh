1a73bc9f7b8326dc202d20879178cca7a72cf0a8
dangoo <daniel.gooss@sinnerschrader.com>
2021-09-16 18:31:07 +0200

    fix(update): cd paths

--- shairport-update.sh
+++ shairport-update.sh
@@ -8,23 +8,24 @@ apt-get upgrade
 # Install nqptp
 echo "Updating nqptp…"
 
-cd nqptp
+cd nqptp/
 git pull
 autoreconf -fi
 ./configure
 make
 make install
+
 cd ~
 
 # Restart nqptp daemon
-echo "Starting nqptp…"
+echo "Restarting nqptp…"
 
 systemctl restart nqptp
 
 # Install shairport-sync
 echo "Updating shairport-sync…"
 
-cd shairport-sync
+cd shairport-sync/
 git pull
 autoreconf -fi
 ./configure --sysconfdir=/etc --with-alsa \
@@ -35,6 +36,6 @@ make install
 cd ~
 
 # Restart shairport-sync
-echo "Starting shairport-sync…"
+echo "Restarting shairport-sync…"
 
 systemctl restart shairport-sync
