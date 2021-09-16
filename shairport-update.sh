e60a662b28980e9c1c4b324fbed6067db3876f9e
dangoo <daniel.gooss@sinnerschrader.com>
2021-09-16 18:39:34 +0200

    feat(update): add diff check

--- shairport-update.sh
+++ shairport-update.sh
@@ -8,14 +8,21 @@ apt-get upgrade
 # Install nqptp
 echo "Updating nqptp…"
 
-cd nqptp/
-git pull
-autoreconf -fi
-./configure
-make
-make install
+cd ~/nqptp/
 
-cd ~
+git fetch
+
+if $(git rev-parse HEAD) != $(git rev-parse @{u}); then
+    git pull
+    autoreconf -fi
+    ./configure
+    make
+    make install
+    echo "Updated successfully"
+
+else
+    echo "Already up-to-date"
+fi
 
 # Restart nqptp daemon
 echo "Restarting nqptp…"
@@ -25,17 +32,24 @@ systemctl restart nqptp
 # Install shairport-sync
 echo "Updating shairport-sync…"
 
-cd shairport-sync/
-git pull
-autoreconf -fi
-./configure --sysconfdir=/etc --with-alsa \
-    --with-soxr --with-avahi --with-ssl=openssl --with-systemd --with-dbus-interface --with-airplay-2
-make -j
-make install
+cd ~/shairport-sync/
 
-cd ~
+if $(git rev-parse HEAD) != $(git rev-parse @{u}); then
+    git pull
+    autoreconf -fi
+    ./configure --sysconfdir=/etc --with-alsa \
+        --with-soxr --with-avahi --with-ssl=openssl --with-systemd --with-dbus-interface --with-airplay-2
+    make -j
+    make install
+    echo "Updated successfully"
+
+else
+    echo "Already up-to-date"
+fi
 
 # Restart shairport-sync
 echo "Restarting shairport-sync…"
 
 systemctl restart shairport-sync
+
+cd ~
